303783cb1d1c1798498362f02417283f726357fa
ZIA UDDIN BABLU <107132736+ziauddin128@users.noreply.github.com>
2024-06-05 20:57:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "ziauddin" //string
+let age = 24 // number
+let is_logged_in = true // boolean
+let state = null //object
+let phone; //undefined
+
+//string
+//Number
+//boolean - true/false
+//object
+//undefined 
