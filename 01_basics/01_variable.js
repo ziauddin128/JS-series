0031e26af260a1b9f659c144358548c742578588
ZIA UDDIN BABLU <107132736+ziauddin128@users.noreply.github.com>
2024-06-05 20:34:53 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 828282;
+let accountHolder = "Kamal Hossain";
+var accountBranch = "Feni"
+
+//accountId = 5252525;
+accountHolder = "Birbal Khan"
+accountBranch = "Dhaka"
+
+/*
+Do not use var in JS because of block scope, always use, const and let;
+*/
+console.table([accountId, accountHolder, accountBranch]);
+
+
