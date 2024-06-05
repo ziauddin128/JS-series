1d0b236ad6344069b1c2fd3a167a9c01b19275f5
ZIA UDDIN BABLU <107132736+ziauddin128@users.noreply.github.com>
2024-06-05 21:23:05 +0000

    conversion_operation

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,46 @@
+//Datatype convert
+
+//Number convert 
+let age = undefined;
+let age_in_number = Number(age);
+
+//console.log(typeof age_in_number);
+
+
+//33 => number
+//"33"=> number
+// "33a" => number - confusion
+// null => number - confusion
+//undefined => number - confusion
+
+//String Convert 
+let name = undefined;
+let name_in_string = String(name);
+
+//console.log(typeof name_in_string);
+//console.log(name_in_string);
+
+// "kamal" => string
+// 33 => string 
+// null => string
+//undefined => string 
+
+
+//Boolean conver
+let logged_in = undefined;
+let logged_in_boolean = Boolean(logged_in);
+
+//console.log(typeof logged_in_boolean);
+//console.log(logged_in_boolean);
+
+// 1 = true, 0 = false
+// "kamal" = true 
+// 12 => true
+// null => false
+// undefined => false 
+
+
+
+
+
+
