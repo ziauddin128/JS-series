06776bdb7d3818b0ac1da2f37fd34bff25703014
ZIA UDDIN BABLU <107132736+ziauddin128@users.noreply.github.com>
2024-06-06 05:23:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+//Comparison means, Compare 2 datatype, Ex: a > b, a = b;
+
+// console.log(2 == 2)
+// console.log(2 > 2)
+// console.log(2 < 2)
+// console.log(2 != 2)
+
+// advance
+// console.log(null == 0) //false
+// console.log(null > 0)  //false
+// console.log(null >= 0) //true
+
+/*
+ Compare treat null as 0
+*/
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+/*
+Equality check and Comparison both different things
+*/
