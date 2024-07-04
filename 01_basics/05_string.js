541bae6f8ea01ec6b7050682219fc54ff4dcb3b2
ZIA UDDIN BABLU <107132736+ziauddin128@users.noreply.github.com>
2024-07-04 20:54:29 +0000

    Strind

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const gameName = new String('Jahid and Karim are good boy');
+//console.log(gameName.charAt(0));
+
+//console.log(gameName.includes('Kiddy'));
+
+//console.log(gameName.indexOf('y'));
+
+//console.log(gameName.replace('Karim','Zia'));
+
+//console.log(gameName.slice(0,5));
+
+console.log(gameName.split(' '));
+
