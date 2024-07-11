0e0ce76e3eb2da1c5c62fc7d4cc19d8a27fd183f
ZIA UDDIN BABLU <107132736+ziauddin128@users.noreply.github.com>
2024-07-11 22:34:14 +0000

    num_math

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,33 @@
+let score = 400;
+//console.log(score);
+
+let anotherNumber = 147570;
+//console.log(anotherNumber.toLocaleString('en-IN'));
+
+let abc_score = 21.45
+//console.log(abc_score.toPrecision(1));
+
+
+/*-- Math */
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,2,5,6,7));
+// console.log(Math.max(1,2,5,6,7));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+//Min And Max 
+let min = 10;
+let max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
